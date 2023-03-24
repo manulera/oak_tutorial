87b80bbc4a6994efcd04b70d237ad99fed9bc8ff
Manuel Lera Ramirez <manulera14@gmail.com>
2023-03-24 09:43:56 +0000

    remove old code

--- pombe_enrichment.sh
+++ pombe_enrichment.sh
@@ -1,10 +1,3 @@
-curl -k https://www.pombase.org/data/annotations/Phenotype_annotations/phenotype_annotations.pombase.phaf.gz --output  input/phenotype_annotations.phaf.gz
-gzip -fd input/phenotype_annotations.phaf.gz
-curl -k https://www.pombase.org/data/annotations/Gene_ontology/gene_association.pombase.gz --output input/go_annotations.gaf.gz
-gzip -fd input/go_annotations.gaf.gz
-
-python format_for_enrichment.py
-
 runoak -i sqlite:obo:fypo -G hpoa -g input/pombe_phenotypes.tsv enrichment -p i -U pombe_genes.tsv -B input/background_protein_coding.tsv -O csv -o enrichment.csv
 
 runoak -i sqlite:obo:go -G gaf -g input/go_annotations.gaf enrichment -p i -U pombe_genes.tsv -B input/background_protein_coding.tsv -O csv -o go_enrichment.csv
